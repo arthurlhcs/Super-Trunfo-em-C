f188ab2c4ec79ccf782f58844a3027d76bf05d56
arthurlhcs <oarthurlhcs@gmail.com>
2025-04-01 19:06:15 -0300

    Adicionei mais informações para as cartas
    Adicionei calculos para a densidade populacional e PIB per capita, criei uma branch para testar melhor o código depois e ver possíveis erros, assim não perco a base da minha master.

--- supertrunfo_iniciante.c
+++ supertrunfo_iniciante.c
@@ -16,6 +16,13 @@ int main(){
 
     int Pontos_turisticos01 = 0, Pontos_turisticos02 = 0;
 
+    // Váriaveis para armazenar os resultados das contas de Densidade Populacional e Pib per Capita.
+
+    float Densidade_Populacional01 = 0, PIB_Percaptacarta01 = 0;
+
+    float Densidade_Populacional02 = 0, PIB_Percaptacarta02 = 0;
+
+
     /* Interação de entrada e saida de dados com o usuário de forma bem sucinta, direta, evitando ambiguidades e possíveis confusões de interpretação, deixando a interação leve e usual. 
        Todos as linhas bem espaçadas e organizadas para melhor compreensão de quem irá manusear o código para futuras manutenções de melhorias do código */
     
@@ -23,7 +30,7 @@ int main(){
 
     printf("\nDigite uma letra de 'A' a 'H' para representar um dos oito estados. ");
     scanf(" %c", &Estado_01);    // Foi utilizado um 'espaço' antes do comando '%c' para burlar o buffer que a tecla 'enter' deixa, deixando assim o caracter ser lido pelo programa.
-
+    
     printf("\nEscolha um código para a carta de 01 a 04: ");
     scanf("%s", Codigo_carta01);
 
@@ -69,6 +76,14 @@ int main(){
     printf("\nDados da carta número 2 cadastrados com sucesso! \n"); // Mensagem de sucesso, indicando ao usuário que tudo ocorreu bem.
     printf("Exibindo os dados cadastrados... \n");
 
+    // Operações com base nos dados fornecidos 
+
+    Densidade_Populacional01 = (float)Habitantes_carta01 / Area_carta01;
+    PIB_Percaptacarta01 = PIB_carta01 / (float)Habitantes_carta01;
+                                                                               // Casting feito nas váriaveis habitantes pois a mesma é do tipo inteiro, podendo resultar em erros nos resultados.
+    Densidade_Populacional02 = (float)Habitantes_carta02 / Area_carta02;
+    PIB_Percaptacarta02 = PIB_carta02 / (float)Habitantes_carta02;
+
     // Exibição dos dados indicados pelo usuário de forma sequencial de acordo com os dados fornecido. 
 
     printf("\nCarta 1: \n");
@@ -79,6 +94,8 @@ int main(){
     printf("Área: %.2f km² \n", Area_carta01);
     printf("PIB: %.2f bilhões de reais \n", PIB_carta01);
     printf("Número de Pontos Turísticos: %d \n", Pontos_turisticos01);
+    printf("Densidade Populacional: %.2f hab/km² \n", Densidade_Populacional01);
+    printf("PIB per Capita: %.2f reais \n", PIB_Percaptacarta01);
 
     printf("\nCarta 2: \n");
     printf("Estado: %c \n", Estado_02);
@@ -87,6 +104,8 @@ int main(){
     printf("População: %d \n", Habitantes_carta02);
     printf("Área: %.2f km² \n", Area_carta02);
     printf("PIB: %.2f bilhões de reais \n", PIB_carta02);
-    printf("Número de Pontos Turísticos: %d ", Pontos_turisticos02);
+    printf("Número de Pontos Turísticos: %d \n", Pontos_turisticos02);
+    printf("Densidade Populacional: %.2f hab/km² \n", Densidade_Populacional02);
+    printf("PIB per Capita: %.2f reais \n", PIB_Percaptacarta02);
 
 }
